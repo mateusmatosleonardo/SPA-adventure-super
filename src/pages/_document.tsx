03e1d763d3696c547b202453f99b720e61676bce
mateus <m4teusmatos@gmail.com>
2023-07-07 21:28:49 -0300

    style: adding bg in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Head, Html, NextScript } from 'next/document'
-
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
   return (
-    <Html lang="en">
+    <Html lang="en" >
       <Head />
       <body>
-
         <NextScript />
+        <Main />
       </body>
     </Html>
   )
